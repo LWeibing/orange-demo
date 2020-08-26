187710e779119a22b990544bfba6113e636b5dbb
ISol <599304776@qq.com>
2020-08-26 16:50:47 +0800

    完成 row 和 col

---
+++ src/col.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default  {
+    name : 'OrangeCol',
+    props: {
+      span : [Number,String]
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  .col {
+    border: 1px solid red;
+    width: 50%;
+    background: gray;
+    height: 100px;
+
+    $class-prefix: col-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n/24) * 100%;
+      }
+    }
+  }
+</style>
