0113e3bfdbea04988ad454fb7637bd581e88eefd
ISol <599304776@qq.com>
2020-08-27 10:48:52 +0800

    基本实现 row 和 col 功能

--- src/col.vue
+++ src/col.vue
@@ -1,27 +1,39 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
-    <slot></slot>
+  <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+  :style="{paddingRight: gutter/2+'px',paddingLeft: gutter/2+ 'px'}">
+    <div style="border: 1px solid green; height: 100px">
+      <slot></slot>
+    </div>
   </div>
 </template>
 <script>
   export default  {
     name : 'OrangeCol',
+    data(){
+      return {
+        gutter: 0
+      }
+    },
     props: {
-      span : [Number,String]
+      span : [Number,String],
+      offset: [Number,String],
     }
   }
 </script>
 <style lang="scss" scoped>
   .col {
-    border: 1px solid red;
     width: 50%;
-    background: gray;
-    height: 100px;
-
     $class-prefix: col-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
         width: ($n/24) * 100%;
+      }
+    }
+
+    $class-prefix: offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n/24) * 100%;
       }
     }
   }
--- src/row.vue
+++ src/row.vue
@@ -1,9 +1,23 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginRight:-gutter/2+'px',marginLeft:-gutter/2+'px'}">
     <slot></slot>
   </div>
 </template>
-
+<script>
+  export default {
+    name: 'OrangeRow',
+    props: {
+      gutter: {
+        type: [Number,String]
+      }
+    },
+    mounted() {
+      this.$children.forEach((vm)=>{
+        vm.gutter = this.gutter
+      })
+    }
+  }
+</script>
 <style lang="scss" scoped>
   .row {
     display: flex;
