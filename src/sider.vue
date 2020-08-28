a67c36718713971c7533abc8e899cc72bcfc35ef
ISol <599304776@qq.com>
2020-08-28 12:10:32 +0800

    添加过渡动画

--- src/sider.vue
+++ src/sider.vue
@@ -1,17 +1,41 @@
 <template>
-  <div class="sider">
-    <slot></slot>
-  </div>
+  <transition name="slide">
+    <div class="sider" v-if="visible">
+      <slot></slot>
+      <button @click="visible=false">close</button>
+    </div>
+  </transition>
 </template>
 
 <script>
   export default {
-    name: 'OrangeSider'
+    data() {
+      return {
+        visible: true
+      }
+    },
+    name: "OrangeSider"
   }
 </script>
 
 <style lang="scss" scoped>
-    .sider{
+  .sider {
+    position: relative;
 
+    > button {
+      position: absolute;
+      top: 0;
+      right: 0;
     }
+  }
+
+  .slide-enter-active, .slide-leave-active {
+    transition: all .5s;
+  }
+
+  .slide-enter, .slide-leave-to /* .fade-leave-active below version 2.1.8 */
+  {
+    margin-left: -200px;
+  }
+
 </style>
