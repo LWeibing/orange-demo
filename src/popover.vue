a3b1394755c58c8551ccb7eee1422dda5f052765
ISol <599304776@qq.com>
2020-08-31 14:53:28 +0800

    把 popover 的 content 放到 外面

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="popover" @click.stop="xxx">
-    <div class="content-wrapper" v-if="visible" @click.stop>
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible" >
       <slot name="content"></slot>
     </div>
-    <slot></slot>
+    <span ref="triggerWrapper">
+          <slot></slot>
+    </span>
   </div>
 </template>
 
@@ -18,6 +20,10 @@
         this.visible = !this.visible
         if(this.visible === true){
           this.$nextTick(()=>{
+            document.body.appendChild(this.$refs.contentWrapper)
+            let {top,left} = this.$refs.triggerWrapper.getBoundingClientRect()
+            this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+            this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
             let eventHandler = ()=>{
               this.visible = false
               document.removeEventListener('click',eventHandler)
@@ -26,7 +32,7 @@
           })
         }
       }
-    }
+    },
   }
 </script>
 
@@ -35,12 +41,11 @@
     display: inline-block;
     vertical-align: top;
     position: relative;
-    .content-wrapper {
-      position: absolute;
-      bottom: 100%;
-      box-shadow: 0 0 3px rgba(0,0,0,.5);
-      left: 0;
-      border: 1px solid red;
-    }
+  }
+  .content-wrapper {
+    position: absolute;
+    box-shadow: 0 0 3px rgba(0,0,0,.5);
+    border: 1px solid red;
+    transform: translateY(-100%);
   }
 </style>
